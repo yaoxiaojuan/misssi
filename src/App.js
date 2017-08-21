f21d25118afaa44864545721a30b98632a5bef7d
Yao Xiaojuan <yaoxiaojuan@cloudin.cn>
2017-08-21 23:25:26 +0800

    布局done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Tag } from 'antd';
+import { Layout, Menu, Icon } from 'antd';
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
+const { Header, Content } = Layout;
 
-class App extends Component {
+export default class App extends Component {
+  state = {
+    current: 'mail',
+  }
+  handleClick = (e) => {
+    console.log('click ', e);
+    this.setState({
+      current: e.key,
+    });
+  }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <Tag>Tag 1</Tag>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Layout>
+        <Menu
+          onClick={this.handleClick}
+          selectedKeys={[this.state.current]}
+          mode="horizontal"
+        >
+          <div className="logo">LuckyYao</div>
+          <Menu.Item key="mail">
+            <Icon type="mail" />技术
+          </Menu.Item>
+          <Menu.Item key="app" disabled>
+            <Icon type="appstore" />小事
+          </Menu.Item>
+        </Menu>
+        <Layout>
+          <Content style={{ padding: 24, background: '#fff', minHeight: '100%' }}>
+            <h1>欢迎来到LuckyYao!</h1>
+            <p>Enjoy yourself!</p>
+          </Content>
+        </Layout>
+      </Layout>
     );
   }
 }
-
-export default App;
