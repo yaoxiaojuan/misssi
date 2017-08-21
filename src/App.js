abd0a95c5c4b6a94995158713d623dfc522818af
Yao Xiaojuan <yaoxiaojuan@cloudin.cn>
2017-08-21 21:58:58 +0800

    installed antd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Tag } from 'antd';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
+          <Tag>Tag 1</Tag>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
