c9b44c930aefbecf208a8c9333e008b916b01d21
Yao Xiaojuan <yaoxiaojuan@cloudin.cn>
2017-08-24 19:52:06 +0800

    响应式样式处理

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -5,7 +5,7 @@ export default class Footer extends Component {
   render() {
     return (
       <footer>
-        <p className="dark">耶和华是我的牧者，我必不至缺乏。 摘自《圣经·诗篇23》</p>
+        <p className="dark">耶和华是我的牧者，我必不至缺乏。<span>摘自《圣经·诗篇23》</span></p>
       </footer>
     );
   }
