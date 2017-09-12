fa258d0a6ef730a635c8225bd289713b6fe9e09e
Yao Xiaojuan <yaoxiaojuan@cloudin.cn>
2017-09-12 19:40:46 +0800

    HTML转图片文字done

--- src/js/article/list.js
+++ src/js/article/list.js
@@ -3,15 +3,22 @@ import demo20170829 from './demo20170829.md';
 import article20170831 from './article20170831.md';
 // import article20170905 from './article20170905.md';
 import article20170906 from './article20170906.md';
-import article20170910 from './article20170910.md';
+// import article20170910 from './article20170910.md';
+import article20170911 from './article20170911.md';
 
 export const articles = {
-  'article20170910': {
-    title: "《启示录6-7章》听道笔记：耶稣的主权和世界的苦难",
-    info: "我看见羔羊揭开七印中第一印的时候。。。。羔羊解开第七印的时候，天上寂静约有二刻。",
-    date: "2017-09-10 9:00PM",
-    content: article20170910
+  'article20170911': {
+    title: "HTML页面转图片",
+    info: "把页面的一部分DOM结构转成图片，并下载下来。",
+    date: "2017-09-11 8:00PM",
+    content: article20170911
   },
+  // 'article20170910': {
+  //   title: "《启示录6-7章》听道笔记：耶稣的主权和世界的苦难",
+  //   info: "我看见羔羊揭开七印中第一印的时候。。。。羔羊解开第七印的时候，天上寂静约有二刻。",
+  //   date: "2017-09-10 9:00PM",
+  //   content: article20170910
+  // },
   // 'article20170905': {
   //   title: "读书：《理家理心》《过犹不及》《但愿婚前我知道》",
   //   info: "用心探索，一系列书籍实践，不单是整理生活空间更是整理生命空间。",
