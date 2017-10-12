94c13359a654495a4c225f1f4728db6a33cfb320
Yao Xiaojuan <yaoxiaojuan@cloudin.cn>
2017-10-12 10:26:53 +0800

    添加slider组件入口

--- src/js/demo/components/Index.js
+++ src/js/demo/components/Index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Switch from './Switch';
+import Slider from './Slider';
 
 export default class Components extends Component {
   render() {
@@ -7,6 +8,8 @@ export default class Components extends Component {
       <section>
         <h3>开关按钮</h3>
         <Switch/>
+        <h3>拖动条</h3>
+        <Slider/>
       </section>
     );
   }
