7afdf2f9a4ae58b518bf529201a33b818ce246ac
Yao Xiaojuan <yaoxiaojuan@cloudin.cn>
2018-01-25 20:15:39 +0800

    git gulp sass & stylelint

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,32 @@
 var gulp = require('gulp');
-var uglify = require('gulp-uglify')
-var minifyCSS = require('gulp-minify-css')
-var imagemin = require('gulp-imagemin')
-const imageminJpegtran = require('imagemin-jpegtran');
+var uglify = require('gulp-uglify');
+var minifyCSS = require('gulp-minify-css');
+var imagemin = require('gulp-imagemin');
+var imageminJpegtran = require('imagemin-jpegtran');
+var sass = require('gulp-ruby-sass');
 
 gulp.task('script', function() {
   gulp.src('assets/js/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('dist/js'));
 });
-gulp.task('css', function () {
-  gulp.src('assets/css/*.css')
-    .pipe(minifyCSS())
-    .pipe(gulp.dest('dist/css'))
-})
+gulp.task('sass', function () {
+    return sass('assets/scss/**/*.scss', {
+      style: 'compressed',
+    })
+    .pipe(gulp.dest('dist/css'));
+});
 gulp.task('images', function () {
   gulp.src('assets/images/*.*')
     .pipe(imagemin({
         progressive: true
     }))
-    .pipe(gulp.dest('dist/images'))
+    .pipe(gulp.dest('dist/images'));
 });
 gulp.task('auto', function () {
-  gulp.watch('assets/js/*.js', ['script'])
-  gulp.watch('assets/css/*.css', ['css'])
-  gulp.watch('assets/images/*.*', ['images'])
+  gulp.watch('assets/js/*.js', ['script']);
+  gulp.watch('assets/scss/**/*.scss', ['sass']);
+  gulp.watch('assets/images/*.*', ['images']);
 });
 
-gulp.task('default', ['images', 'script', 'css', 'auto'])
+gulp.task('default', ['images', 'script', 'sass', 'auto']);
