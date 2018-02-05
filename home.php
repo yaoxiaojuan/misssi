1ff2777838f724dc2affcb404fe883c16b6042d0
Yao Xiaojuan <yaoxiaojuan@cloudin.cn>
2018-02-05 15:46:08 +0800

    loadmore style upgrade

--- home.php
+++ home.php
@@ -1,8 +1,8 @@
 <?php get_header();?>
 <div class="w posts">
   <div class="post_info">
-    <h1>Welcome to the Blog</h1>
-    <p>Neither do people light a lamp and put it under a bowl. <br>Instead they put it on its stand, and it gives light to everyone in the house. </p>
+    <h1>欢迎来到MissSI的博客</h1>
+    <p>人点灯，不放在斗底下, 是放在灯台上，就照亮一家的人, MissSi与你分享 · 微光。</p>
     <?php get_search_form(); ?>
   </div>
   <?php dynamic_sidebar( 'sidebar-1' ); ?>
@@ -11,7 +11,7 @@
       while(have_posts()): the_post();
         get_template_part(('content-article'));
       endwhile;?>
-      <p class="font-miss loadmore text-center pointer margin-20">loadmore</p>
+      <p class="loadmore text-center pointer margin-20">加载更多</p>
     <?php else :
       echo '<p> No content found</p>';
     endif;?>
--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
 <form class="post_form" role="search" action="<?php echo home_url('/'); ?>">
-  <input type="text" placeholder="Looking for something?..." name="s" id="s">
+  <input type="text" placeholder="告诉我，你想看什么文章？" name="s" id="s">
   <input type="submit" id="searchsubmit" value="">
 </form>
