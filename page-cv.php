6ab24ff91b7f126103213ce0afacbf68d1d699ec
Yao Xiaojuan <yaoxiaojuan@cloudin.cn>
2018-02-06 21:39:46 +0800

    upgrade cv style

--- page-cv.php
+++ page-cv.php
@@ -12,19 +12,19 @@
     <div class="info__basic">
       <img class="photo" src="http://ovjyf065b.bkt.clouddn.com/wechat.png" alt="头像">
       <h1 class="name">要笑娟</h1>
-      <a class="intro" href="http://misssi.net">http://misssi.net</a>
+      <p class="font-thin font--size-small">前端工程师 | 2年经验</p>
     </div>
     <div class="padding--bottom-20 info--line">
       <h2 class="info__title">PROFILE</h2>
-      <p class="margin--bottom-15">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit aut deserunt sit, laudantium aliquam quia enim </p>
+      <p class="margin--bottom-15 font--size-small">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit aut deserunt sit, laudantium aliquam quia enim </p>
     </div>
     <div class="info__connect padding--bottom-20 info--line">
       <h2 class="info__title">CONTACT ME</h2>
-      <a href="tel:18230273602">182-3027-3602</a>
-      <a href="mailto:lucky.yaoxiaojuan@gmail.com">lucky.yaoxiaojuan@gmail.com</a>
+      <a class="font--size-small" href="tel:18230273602">182-3027-3602</a>
+      <a class="font--size-small" href="mailto:lucky.yaoxiaojuan@gmail.com">lucky.yaoxiaojuan@gmail.com</a>
       <br>
-      <a href="http://misssi.net">http://misssi.net</a>
-      <a href="https://github.com/luckyYao">https://github.com/luckyyao</a>
+      <a class="font--size-small" href="http://misssi.net">http://misssi.net</a>
+      <a class="font--size-small" href="https://github.com/luckyYao">https://github.com/luckyyao</a>
     </div>
     <div class="padding--bottom-20 info--line">
       <h2 class="info__title">SKILLS</h2>
@@ -56,59 +56,79 @@
     </div>
   </div>
   <div class="content">
-    <div class="content__projects">
-      <h2 class="content__title">项目经验</h2>
-      <ul class="content__list table">
-        <li class="row">
-          <span class="list__label cell">公有云</span>
-          <p class="cell">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, totam, eveniet! Sint mollitia, quo tempora laudantium officia facere ipsam corporis quisquam excepturi, magnam corrupti distinctio qui sapiente dolore ullam voluptate?</p>
-        </li>
-        <li class="row">
-          <span class="list__label cell">公有云</span>
-          <p class="cell">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, totam, eveniet! Sint mollitia, quo tempora laudantium officia facere ipsam corporis quisquam excepturi, magnam corrupti distinctio qui sapiente dolore ullam voluptate?</p>
-        </li>
-      </ul>
-    </div>
-    <div class="content__jobs">
-      <h2 class="content__title">工作经验</h2>
-      <div class="content__list table">
-        <h3 class="row">
-          <span class="list__label cell">Feb 2016-Feb 2018</span>
-          <a class="cell" href="http://cloudin.cn/">Cloudin云英</a>
-        </h3>
-        <ul>
-          <li class="row">
-            <span class="list__label cell">公有云</span>
-            <p class="cell">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, totam, eveniet! Sint mollitia, quo tempora laudantium officia facere ipsam corporis quisquam excepturi, magnam corrupti distinctio qui sapiente dolore ullam voluptate?</p>
-          </li>
-          <li class="row">
-            <span class="list__label cell">公有云</span>
-            <p class="cell">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, totam, eveniet! Sint mollitia, quo tempora laudantium officia facere ipsam corporis quisquam excepturi, magnam corrupti distinctio qui sapiente dolore ullam voluptate?</p>
-          </li>
-        </ul>
+    <h2 class="content__title">项目经验</h2>
+    <div class="table">
+      <div class="row">
+        <a href="http://misssi.net" class="cell">Wordpress主题</a>
+        <p class="cell">拆箱即用的Wordpress主题，很漂亮哦，喜欢你就用，使用方法写得也很清楚哦。</p>
       </div>
-      <div class="job">
-        <h3 class="job__title">
-          <span class="inline-block margin--right-20">Feb 2016-Feb 2018</span>
-          <a class="inline-block" href="http://cloudin.cn/">大学生创业</a>
-        </h3>
-        <ul class="job__info table">
-          <li class="row">
-            <span class="list__label cell">公有云</span>
-            <p class="cell">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, totam, eveniet! Sint mollitia, quo tempora laudantium officia facere ipsam corporis quisquam excepturi, magnam corrupti distinctio qui sapiente dolore ullam voluptate?</p>
-          </li>
-          <li class="row">
-            <span class="list__label cell">公有云</span>
-            <p class="cell">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, totam, eveniet! Sint mollitia, quo tempora laudantium officia facere ipsam corporis quisquam excepturi, magnam corrupti distinctio qui sapiente dolore ullam voluptate?</p>
-          </li>
-        </ul>
+      <div class="row">
+        <a href="" class="cell">Protect守护者</a>
+        <p class="cell">帮助抑郁症患者走出困境，里面有测试，抑郁症介绍文章和抑郁症患者饮食建议。帮助大家了解抑郁症。
+        适合抑郁症患者和家人使用的APP。</p>
+      </div>
+      <div class="row">
+        <a href="" class="cell">完美弧线</a>
+        <p class="cell">购物车抛物线效果，删除文件进垃圾桶效果都可以用这个小插件实现，轻便易用，按着文档操作，3分钟即可实现想要的效果。</p>
+      </div>
+      <div class="row">
+        <a href="" class="cell">Tree</a>
+        <p class="cell">支持无限级拓展的树结构，比赛对战图，组织架构轻松实现。</p>
+      </div>
+      <div class="row">
+        <a href="" class="cell">下雪的春节</a>
+        <p class="cell">回忆中的最美的春节，在雪地里穿着新衣服，放鞭炮，打雪仗。</p>
+      </div>
+      <div class="row">
+        <a href="" class="cell"></a>
+        <a href="" class="cell text-right font--size-small">更多项目，请移步Github...</a>
       </div>
     </div>
-    <div class="content__edu">
-      <p>燕山大学</p>
-      <p>软件工程学院·信息安全</p>
-      <p>2012-2016</p>
-      <p>英语6级</p>
+    <h2 class="content__title">工作经验</h2>
+    <div class="table">
+      <h3 class="row content__title2">
+        <span class="cell font--color-main">Feb 2016-Feb 2018</span>
+        <span class="cell font-normal">Cloudin云英</span>
+      </h3>
+      <div class="row">
+        <span class="cell">公有云</span>
+        <p class="cell">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, totam, eveniet! Sint mollitia, quo tempora laudantium officia facere ipsam corporis quisquam excepturi, magnam corrupti distinctio qui sapiente dolore ullam voluptate?</p>
+      </div>
+      <div class="row">
+        <span class="cell">公有云</span>
+        <p class="cell">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, totam, eveniet! Sint mollitia, quo tempora laudantium officia facere ipsam corporis quisquam excepturi, magnam corrupti distinctio qui sapiente dolore ullam voluptate?</p>
+      </div>
+      <h3 class="row content__title2">
+        <span class="cell font--color-main">Feb 2016-Feb 2018</span>
+        <span class="cell">大学生创业</span>
+      </h3>
+      <div class="row">
+        <span class="cell">公有云</span>
+        <p class="cell">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, totam, eveniet! Sint mollitia, quo tempora laudantium officia facere ipsam corporis quisquam excepturi, magnam corrupti distinctio qui sapiente dolore ullam voluptate?</p>
+      </div>
+      <div class="row">
+        <span class="cell">公有云</span>
+        <p class="cell">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, totam, eveniet! Sint mollitia, quo tempora laudantium officia facere ipsam corporis quisquam excepturi, magnam corrupti distinctio qui sapiente dolore ullam voluptate?</p>
+      </div>
+    </div>
+    <h2 class="content__title">教育经历</h2>
+    <div class="table margin--bottom-80">
+      <div class="row content__title2">
+        <span class="cell font--color-main">Sep 2012-Jul 2016</span>
+        <span class="cell">燕山大学</span>
+      </div>
+      <div class="row">
+        <span class="cell">信息安全</span>
+        <span class="cell">学习成绩很好的，英语六级学习成绩很好的，英语六级学习成绩很好的，英语六级。</span>
+      </div>
+      <div class="row">
+        <a href="" class="cell"></a>
+        <p class="cell text-right font--size-small">
+          <a href="http://misssi.net">了解更多？请移步Blog...</a>
+          <span class="font--color-main">&nbsp;or&nbsp;</span>
+          <a href="">下载简历</a>
+        </p>
+      </div>
     </div>
   </div>
 </section>
